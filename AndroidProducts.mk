1af589a83b206de735df04a2f55050131d2f5318
Amy Grace <00p513@protonmail.com>
2020-09-18 11:35:47 +0100

    lineage to revenge

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_channel.mk
+    $(LOCAL_DIR)/revengeos_channel.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_channel-eng \
-    lineage_channel-user \
-    lineage_channel-userdebug
+    revengeos_channel-eng \
+    revengeos_channel-user \
+    revengeos_channel-userdebug
